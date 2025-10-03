e5c1ef3e3a2b3d20dc25683435b1e1d948401e64
dmenezes <148342102+ogdie@users.noreply.github.com>
2025-10-03 01:00:41 +0100

    commit atualizado

--- src/components/CardEstatistica.jsx
+++ src/components/CardEstatistica.jsx
@@ -1,7 +1,7 @@
 export default function CardEstatistica({ titulo, descricao }) {
   return (
     <div className="border rounded p-4 mb-2 shadow">
-      <h2>{titulo}</h2>
+      <h2 className="font-semibold">{titulo}</h2>
       <p>{descricao}</p>
     </div>
   );
